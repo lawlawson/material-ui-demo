98709c02716212fccb1a70db8dad774ea912ea1e
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-03-02 17:51:02 +0000

    Added all necessary imports and deleted unused fies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Typography, AppBar, Card, CardACtions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@material-ui/core'
+import PhotoCamera from '@material-ui/icons'
+
+const App = () => {
+  return(
+    <>
+    </>
+  )
+}
+
+export default App
