19022b294a1a6d3aac539a46698d2b442be7b8a1
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-03-03 01:11:01 +0000

    Added separate styles file and styled hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
-import {Typography, AppBar, Card, CardACtions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@material-ui/core'
-import PhotoCamera from '@material-ui/icons'
+import {Typography, AppBar, Button, CardACtions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@material-ui/core'
+import {PhotoCamera} from '@material-ui/icons'
+
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
+
   return(
     <>
+      <CssBaseline />
+      <AppBar position={"relative"}>
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant={"h6"}>
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth={"sm"}>
+            <Typography variant={"h2"} align={"center"} color={"textPrimary"} gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant={"h5"} align={"center"} color={"textSecondary"} paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis consequuntur culpa ea id magni qui, sed unde vero voluptas!
+            </Typography>
+          </Container>
+        </div>
+        <Grid container spacing={2} justify={"center"}>
+          <Grid item>
+            <Button variant={"contained"} color={"primary"}>
+              See my photos
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant={"outlined"} color={"primary"}>
+              Secondary action
+            </Button>
+          </Grid>
+        </Grid>
+      </main>
     </>
   )
 }
